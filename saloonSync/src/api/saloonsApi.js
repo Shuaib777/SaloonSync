e625a7c695580ba03642b3ba367cb25545d7a596
Shuaib777 <shuaibahmedshaikh7@gmail.com>
2024-10-31 03:20:51 +0530

    added

--- saloonSync/src/api/saloonsApi.js
+++ saloonSync/src/api/saloonsApi.js
@@ -1,12 +1,9 @@
 import axios from "axios";
-import API_URL from "../config/apiConfig.js";
 
 export const fetchSaloons = async (endpoint = "", params) => {
   try {
-    const domain = process.env.NODE_ENV === "production" ? API_URL : "/api";
-    const url = `${domain}/saloons/${endpoint}`;
+    const url = `https://saloonsync.onrender.com/saloons/${endpoint}`;
     const saloons = await axios.get(url, { params });
-    console.log(saloons);
     return saloons;
   } catch (err) {
     return err;
--- saloonSync/src/config/apiConfig.js
+++
@@ -1,3 +0,0 @@
-const API_URL = "https://saloonsync.onrender.com";
-
-export default API_URL;
